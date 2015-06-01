05e44cd820e91e0c07e0f862f4db691756314c0d
Vinícius Krolow <krolow@gmail.com>
2015-05-31 22:42:30 -0300

    Adding api to track movement and convert to units

--- example/track-mouse.js
+++ example/track-mouse.js
@@ -1,20 +1,5 @@
-import mouse from 'mouse-movement';
+var mouseMovement = require('../');
 
-mouse.every(1).cm(function (x, y) {
-
+mouseMovement.every(20).mt(function (x, y) {
+  console.log('100 mt moved!');
 });
-
-mouse.track(function (x, y) {
-  console.log('mouse movement!');
-  console.log(x, y);
-});
-
-mouse.move(x, y);
-
-mouse.every(2).mm(function (x, y) {
-
-});
-
-mouse.every(1).km(function (x, y) {
-
-})
--- index.js
+++ index.js
@@ -1,34 +1,5 @@
-var mouse = require('./build/Release/mouse.node');
-
-mouse(function(x, y){
-  console.log(x, y); // 'hello world'
+require('babel/register')({
+  stage: 0,
 });
 
-// export default {
-//
-//   every: function (number) {
-//     this.number = number;
-//   },
-//
-//   px: function (callback) {
-//
-//   }
-//
-//   mm: function (callback) {
-//
-//   }
-//
-//   cm: function (callback) {
-//
-//   }
-//
-//   mt: function (callback) {
-//
-//   }
-//
-//   track: function (callback) {
-//     const point = mouse.track();
-//
-//     callback()
-//   }
-// }
+module.exports = require('./lib');
