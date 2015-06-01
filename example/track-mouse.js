c54f1a08a4f0ad083530461caf0f34a87db14925
Vinícius Krolow <krolow@gmail.com>
2015-05-31 22:45:10 -0300

    Adding more examples

--- example/track-mouse.js
+++ example/track-mouse.js
@@ -1,5 +1,29 @@
 var mouseMovement = require('../');
 
 mouseMovement.every(20).mt(function (x, y) {
-  console.log('100 mt moved!');
+  console.log('20 mt moved!');
 });
+
+mouseMovement.every(1).km(function (x, y) {
+  console.log('1 km moved!');
+});
+
+mouseMovement.every(1).cm(function (x, y) {
+  console.log('1 cm moved!');
+});
+
+mouseMovement.every(1).mm(function (x, y) {
+  console.log('1 mm moved!');
+});
+
+mouseMovement.every(100).px(function (x, y) {
+  console.log('100 px moved!');
+});
+
+mouseMovement.track(function (x, y) {
+  console.log(x, y);
+});
+
+
+
+
